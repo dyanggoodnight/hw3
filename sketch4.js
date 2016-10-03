9b488ac585828e91082109acf0eb8848619e4539
dyanggoodnight <dyang@cca.edu>
2016-10-03 11:28:47 -0700

    Create sketch4.js

---
+++ sketch4.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(820, 820);
+  background(255);
+}
+
+function draw() {
+  stroke(0);
+  for (var i = 20; i <= 400; i = i + 5) {
+    var startX = 400 - i;
+    var startY = i;
+    var endX = 400 + i;
+    var endY = i;
+    line(startX, startY, endX, endY);
+  }
+  for (var i = 20; i <= 400; i = i + 5) {
+    var startXX = i;
+    var startYY = 400 + i;
+    var endXX = 800 - i;
+    var endYY = 400 + i;
+    line(startXX, startYY, endXX, endYY);
+  }
+}
